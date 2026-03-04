2e252d69e50e298b6ef80a219ba34f9e701a037f
Magyar Márk <markmagyar2003@gmail.com>
2026-03-04 16:07:32 +0100

    javascript kezdés

---
+++ 03-04/jsbase/app.js
@@ -0,0 +1,6 @@
+console.log("Működik")
+
+console.log(typeof [3, 5, 2])
+console.log(typeof (3 < 5))
+console.log(typeof function(){})
+console.log(typeof (() => {}))
---
+++ 03-04/jsbase/valtozo.js
@@ -0,0 +1,12 @@
+let nev = 'Béla'
+console.log(nev)
+console.log(typeof nev)
+
+var csinal = () => {
+    console.log('függvényen belül...')
+    const a = 35;
+}
+
+csinal()
+a = document.getElementById('vmi')
+console.log(a)
