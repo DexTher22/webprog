a084e7d41fc04d8107835180dcbc2e4915fcfb82
Magyar Márk <markmagyar2003@gmail.com>
2026-03-04 16:09:23 +0100

    na

--- 03-04/jsbase/valtozo.js
+++ 03-04/jsbase/valtozo.js
@@ -2,11 +2,20 @@ let nev = 'Béla'
 console.log(nev)
 console.log(typeof nev)
 
-var csinal = () => {
-    console.log('függvényen belül...')
-    const a = 35;
-}
+// var csinal = () => {
+//     console.log('függvényen belül...')
+//     const a = 35;
+// }
 
-csinal()
-a = document.getElementById('vmi')
-console.log(a)
+// csinal()
+// a = document.getElementById('vmi')
+// console.log(a)
+// const a = document.getElementById('vmi');
+// a.innerHTML = 'Más szöveg'
+
+var kor = 25
+if (kor < 18) {
+    console.log('Nem felelt meg!')
+}else {
+    console.log('oké')
+}
